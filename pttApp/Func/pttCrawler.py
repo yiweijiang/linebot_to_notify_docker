21228a7da641aac0e76beb6d5876c9277fa24cbd
yiweijiang <ua12199413000@gmaill.com>
2022-03-24 20:02:38 +0800

    change ngrok url

--- pttApp/Func/pttCrawler.py
+++ pttApp/Func/pttCrawler.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 from pttApp.models import Ptt_Info
 
 def PTTCrawler():
@@ -7,17 +8,28 @@ def PTTCrawler():
     url = 'https://www.ptt.cc/'
     web = requests.get('https://www.ptt.cc/bbs/Gossiping/index.html', cookies={'over18':'1'})
     soup = BeautifulSoup(web.text, "html.parser")
-    nrecs = soup.find_all('div', class_='nrec')
-    titles = soup.find_all('div', class_='title')
-    for nrec, title in zip(nrecs, titles):
-        if title.find('a') != None and nrec.text.isdigit():
-            if int(nrec.text) >= 50:
-                href = url + title.find('a')['href']
-                if Ptt_Info.objects.filter(URL=href).exists() == False:
-                    res += f"{title.find('a').get_text()}\n{href}\n\n"
-                    # 建立新資料
-                    Ptt_Info.objects.create(URL=href,TITLE=title.find('a').get_text(),NRECS=int(nrec.text))
-                else:
-                    # 更新推數
-                    Ptt_Info.objects.filter(URL=href).update(NRECS=int(nrec.text))
+
+    index = soup.find_all('a', class_='btn wide')[1]['href']
+    page = int(re.findall(r"\d+", index)[0]) + 1
+
+    for i in range(20, -1, -1):
+        page_url = f'https://www.ptt.cc/bbs/Gossiping/index{page-i}.html'
+        r = requests.get(page_url, cookies={'over18':'1'})
+        soup = BeautifulSoup(r.text, "html.parser")
+
+        nrecs = soup.find_all('div', class_='nrec')
+        titles = soup.find_all('div', class_='title')
+        for nrec, title in zip(nrecs, titles):
+            if title.find('a') != None and nrec.text.isdigit():
+                if int(nrec.text) >= 50:
+                    href = url + title.find('a')['href']
+                    if Ptt_Info.objects.filter(URL=href).exists() == False:
+                        res += f"{title.find('a').get_text()}\n{href}\n\n"
+                        # 建立新資料
+                        Ptt_Info.objects.create(URL=href,TITLE=title.find('a').get_text(),NRECS=int(nrec.text))
+                    else:
+                        # 更新推數
+                        Ptt_Info.objects.filter(URL=href).update(NRECS=int(nrec.text))
     return res
+
+# PTTCrawler()
