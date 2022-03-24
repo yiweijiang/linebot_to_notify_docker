20d2f9cb30acbd9d2687cd6db5eb7a18afe9648e
yiweijiang <ua12199413000@gmaill.com>
2022-03-24 16:41:16 +0800

    Add Crawler PTT Gossiping Func

--- pttApp/Func/pttCrawler.py
+++ pttApp/Func/pttCrawler.py
@@ -2,12 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 def PTTCrawler():
-    res = ''
+    res = '\n'
     url = 'https://www.ptt.cc/'
     web = requests.get('https://www.ptt.cc/bbs/Gossiping/index.html', cookies={'over18':'1'})
     soup = BeautifulSoup(web.text, "html.parser")
+    nrecs = soup.find_all('div', class_='nrec')
     titles = soup.find_all('div', class_='title')
-    for i in titles:
-        if i.find('a') != None:
-            res += f"{i.find('a').get_text()}\n{url + i.find('a')['href']}\n\n"
+    for nrec, title in zip(nrecs, titles):
+        if title.find('a') != None and nrec.text.isdigit():
+            if int(nrec.text) >= 50:
+                res += f"{title.find('a').get_text()}\n{url + title.find('a')['href']}\n\n"
     return res
