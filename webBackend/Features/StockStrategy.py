e2adc09dbc63d1f47bf8fd379f52cc294ace475c
yiweijiang <ua12199413000@gmaill.com>
2022-03-31 22:11:29 +0800

    Add Stock Strategy

--- webBackend/Features/StockStrategy.py
+++ webBackend/Features/StockStrategy.py
@@ -1,10 +1,11 @@
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import re
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import os
 
 class StockStrategy():
 	def __init__(self):
